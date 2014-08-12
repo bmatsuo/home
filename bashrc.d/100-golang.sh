504ea81a8e482c30892ad795e4e7252154de1426
Bryan Matsuo <bryan.matsuo@gmail.com>
2014-08-12 00:49:25 -0700

    go: update app_engine path to be consistent with new versions
    the path itself is pretty lame. i don't really know what to do about
    this in the future.

--- bashrc.d/100-golang.sh
+++ bashrc.d/100-golang.sh
@@ -1,3 +1,3 @@
 # $HOME/bin is _expected_ to be added to PATH separately
 export GOPATH="$HOME"
-export PATH="$PATH:$HOME/Go/AppEngine/google_appengine"
+export PATH="$PATH:$HOME/Go/AppEngine/go_appengine"
