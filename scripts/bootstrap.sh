12288379a3317e50bda6a451517ede8f55136b00
Bryan Matsuo <bryan.matsuo@gmail.com>
2014-08-12 05:54:07 -0700

    fix bugs

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/env bash
 
+##
+## usage:
+##  bash scripts/bootstrap.sh
+##  GITHUB_HOME_USE_HTTP=yes GITHUB_HOME_LOCAL="$HOME/path/to/local/repo" bash scripts/bootstrap.sh
+
+GITHUB_HOME_REPO=bmatsuo/home.git
+if [ -z "$GITHUB_HOME_USE_HTTP" ]; then
+    GITHUB_HOME_URL="git@github.com:bmatsuo/home.git"
+else
+    GITHUB_HOME_URL="https://github.com/bmatsuo/home.git"
+fi
+
+echo 'bootstrap install of github.com/bmatsuo/home'
+
 if [ -z `ssh-add -l` ]; then
     echo 'no ssh key is set up' 1>&2
     exit 1
 fi
 
-if [ -z GITHUB_HOME ]; then
-    GITHUB_HOME=~/home
+if [ -z "$GITHUB_HOME_LOCAL" ]; then
+    GITHUB_HOME_LOCAL="$HOME/home"
 fi
+echo "cloning to $GITHUB_HOME_LOCAL"
 
-git clone git@github.com:bmatsuo/home.git "$GITHUB_HOME"
+git clone "$GITHUB_HOME_URL" "$GITHUB_HOME_LOCAL"
 [ $? -eq 0 ] || exit 1
 
-cd "$GITHUB_HOME" && bash scripts/install.sh ${[@]}
+echo "installing files"
+cd "$GITHUB_HOME_LOCAL" && bash scripts/install.sh
