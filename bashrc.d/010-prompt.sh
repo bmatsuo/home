0940b0ed7ddaf1b162c7b59bff6e524de32e506a
Bryan Matsuo <bryan.matsuo@gmail.com>
2017-01-28 01:19:44 -0800

    bash: use a compacted full-path in the PS1 prompt using PROMPT_COMMAND

--- bashrc.d/010-prompt.sh
+++ bashrc.d/010-prompt.sh
@@ -15,10 +15,14 @@ if [ -n "$force_color_prompt" ]; then
 	color_prompt=
     fi
 fi
+
+# Use an abbreviated path in the 
+PROMPT_COMMAND='PS1X=$(p=`pwd`; p=${p#${HOME}}; p=$(echo "$p" | sed "s!\\([^/]\\)[^/][^/][^/][^/]*/!\\1.../!g"); [ p != "$PWD" ] && printf "~";  echo "$p")'
+
 if [ "$color_prompt" = yes ]; then
-    PS1="$prompt_base"'\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+    PS1="$prompt_base"'\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]${PS1X}\[\033[00m\]\$ '
 else
-    PS1="$prompt_base"'\u@\h:\w\$ '
+    PS1="$prompt_base"'\u@\h:${PS1X}\$ '
 fi
 env_unset="color_prompt:force_color_prompt:$env_unset"
 
