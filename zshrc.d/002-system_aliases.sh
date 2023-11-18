b21b3a8dd721c1404fba49a0ca276cf2ddad9255
Bryan Matsuo <bryan.matsuo@gmail.com>
2023-11-17 23:20:19 -0600

    zsh: alias `help` to work like bash

--- zshrc.d/002-system_aliases.sh
+++ zshrc.d/002-system_aliases.sh
@@ -1,3 +1,13 @@
+# alias `help` for shell builtin documentation (like in bash)
+#   help echo
+#   help pushd
+if [ -d "/usr/share/zsh/${ZSH_VERSION}/help" ]; then
+    unalias run-help # run-help is initially an alias for `man`
+    autoload run-help
+    HELPDIR=/usr/share/zsh/"${ZSH_VERSION}"/help
+    alias help=run-help
+fi
+
 # pushd + dirs
 alias dirp="dirs -p"
 alias dirv="dirs -v"
